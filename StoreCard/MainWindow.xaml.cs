f0b25c17205e488ae2cffbb9f3330426e3ad4660
shanedonburke <shanedonburke@gmail.com>
2022-04-11 15:50:30 -0400

    Show "Options" menu item for files and folders

--- StoreCard/MainWindow.xaml.cs
+++ StoreCard/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace StoreCard;
