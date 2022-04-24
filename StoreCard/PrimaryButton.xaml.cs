95f9d66f61e2d17b9de68bd23f8f0fa648d0ef48
shanedonburke <shanedonburke@gmail.com>
2022-04-24 10:58:31 -0400

    Make button text a dependency property

--- StoreCard/PrimaryButton.xaml.cs
+++ StoreCard/PrimaryButton.xaml.cs
@@ -7,12 +7,17 @@ namespace StoreCard
     /// </summary>
     public partial class PrimaryButton
     {
-        public new DependencyProperty IsEnabledProperty = DependencyProperty.Register(
+        public new static readonly DependencyProperty IsEnabledProperty = DependencyProperty.Register(
             nameof(IsEnabled),
             typeof(bool),
             typeof(PrimaryButton));
 
-        public RoutedEvent ClickEvent = EventManager.RegisterRoutedEvent(
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            nameof(Text),
+            typeof(string),
+            typeof(PrimaryButton));
+
+        public static readonly RoutedEvent ClickEvent = EventManager.RegisterRoutedEvent(
             nameof(Click),
             RoutingStrategy.Bubble,
             typeof(RoutedEventHandler),
@@ -22,6 +27,12 @@ namespace StoreCard
         {
             get => (bool) GetValue(IsEnabledProperty);
             set => SetValue(IsEnabledProperty, value);
+        }
+
+        public string Text
+        {
+            get => (string) GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
         }
 
         public event RoutedEventHandler Click
