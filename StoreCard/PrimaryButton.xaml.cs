724df8624ba50788575592378478fb8123af5be8
shanedonburke <shanedonburke@gmail.com>
2022-04-24 10:53:22 -0400

    Create `PrimaryButton` user control

---
+++ StoreCard/PrimaryButton.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StoreCard
+{
+    /// <summary>
+    /// Interaction logic for PrimaryButton.xaml
+    /// </summary>
+    public partial class PrimaryButton
+    {
+        public new DependencyProperty IsEnabledProperty = DependencyProperty.Register(
+            nameof(IsEnabled),
+            typeof(bool),
+            typeof(PrimaryButton));
+
+        public RoutedEvent ClickEvent = EventManager.RegisterRoutedEvent(
+            nameof(Click),
+            RoutingStrategy.Bubble,
+            typeof(RoutedEventHandler),
+            typeof(PrimaryButton));
+
+        public new bool IsEnabled
+        {
+            get => (bool) GetValue(IsEnabledProperty);
+            set => SetValue(IsEnabledProperty, value);
+        }
+
+        public event RoutedEventHandler Click
+        {
+            add => AddHandler(ClickEvent, value);
+            remove => RemoveHandler(ClickEvent, value);
+        }
+
+        public PrimaryButton()
+        {
+            InitializeComponent();
+        }
+
+        private void CustomButton_Click(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(ClickEvent)
+                {RoutedEvent = ClickEvent});
+        }
+    }
+}
