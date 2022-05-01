777c94fa00733548e4fe7b75ca1560fe1b63beaf
shanedonburke <shanedonburke@gmail.com>
2022-05-01 15:20:12 -0400

    Add model classes for Epic games

---
+++ StoreCard/Models/Games/Epic/LauncherInstalled.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreCard.Models.Games.Epic;
+
+public class LauncherInstalled
+{
+    public class InstalledApp
+    {
+        public string InstallLocation;
+
+        public string NamespaceId;
+
+        public string ItemId;
+
+        public string ArtifactId;
+
+        public string AppVersion;
+
+        public string AppName;
+    }
+
+    public List<InstalledApp> InstallationList;
+}
