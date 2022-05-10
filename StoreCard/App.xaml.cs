d55ba30f3c6620ef4bcef0ff1587742b68917ae1
shanedonburke <shanedonburke@gmail.com>
2022-05-10 09:56:06 -0400

    Use Roboto instead of Segoe UI

--- StoreCard/App.xaml.cs
+++ StoreCard/App.xaml.cs
@@ -12,6 +12,13 @@ namespace StoreCard
     /// </summary>
     public partial class App
     {
+        public App()
+        {
+            FrameworkElement.StyleProperty.OverrideMetadata(typeof(Window), new FrameworkPropertyMetadata {
+                DefaultValue = FindResource(typeof(Window))
+            });
+        }
+
         private void App_Startup(object sender, StartupEventArgs e)
         {
             Processes.KillOtherStoreCardProcesses();
