923f2e81fa8c23523ed65d206578a4cc47258b4f
shanedonburke <shanedonburke@gmail.com>
2022-05-20 11:11:07 -0400

    Improve shader compilation script

--- StoreCard/Effects/CompileShaders.bat
+++ StoreCard/Effects/CompileShaders.bat
@@ -1,2 +1,6 @@
-"%ProgramFiles(x86)%\Windows Kits\10\bin\10.0.20348.0\\x86\fxc.exe" /O0 /Fc /Zi /T ps_3_0 /Fo Effects\bin\ColorChange.ps Effects\ColorChange.fx
-del Effects\*.cod
+for %%f in (Effects\Shaders\*.fx) do (
+  echo Compiling %%~nf.fx
+  "%ProgramFiles(x86)%\Windows Kits\10\bin\10.0.22000.0\x86\fxc.exe" /O0 /Fc /Zi /T ps_3_0 /Fo Effects\bin\%%~nf.ps Effects\Shaders\%%~nf.fx
+)
+del Effects\Shaders\*.cod
+echo Finished compiling shaders.
