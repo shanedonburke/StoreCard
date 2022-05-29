7e53ddec43d413a014040a91569d7bb185b6911e
shanedonburke <shanedonburke@gmail.com>
2022-05-29 14:00:02 -0400

    Add docstrings

--- StoreCard/Commands/ShowMissingExecutableAlertCommand.cs
+++ StoreCard/Commands/ShowMissingExecutableAlertCommand.cs
@@ -7,15 +7,37 @@ using StoreCard.Windows;
 
 namespace StoreCard.Commands;
 
+/// <summary>
+/// Shows an alert window indicating that an executable (used to open a file system item)
+/// could not be found.
+/// </summary>
 public sealed class ShowMissingExecutableAlertCommand : IStoreCardCommand<bool>
 {
+    /// <summary>
+    /// The window title.
+    /// </summary>
     private const string WindowTitle = "Missing Executable";
+
+    /// <summary>
+    /// A snippet explaining the alert.
+    /// </summary>
     private const string Explanation = "could not be found";
 
+    /// <summary>
+    /// The item we are trying to open.
+    /// </summary>
     private readonly SavedFileSystemItem _item;
 
+    /// <summary>
+    /// Creates the command.
+    /// </summary>
+    /// <param name="item">The item we are trying to open</param>
     public ShowMissingExecutableAlertCommand(SavedFileSystemItem item) => _item = item;
 
+    /// <summary>
+    /// Opens the window without waiting for it to close.
+    /// </summary>
+    /// <returns>True</returns>
     public bool Execute()
     {
         new InvalidExecutableWindow(_item, WindowTitle, Explanation).Show();
--- StoreCard/Commands/ShowMissingItemAlertCommand.cs
+++ StoreCard/Commands/ShowMissingItemAlertCommand.cs
@@ -8,15 +8,42 @@ using StoreCard.Windows;
 
 namespace StoreCard.Commands;
 
+/// <summary>
+/// Opens an alert window indicating that the item we are trying to open does not exist
+/// (applicable to file system and executable items).
+/// </summary>
 public sealed class ShowMissingItemAlertCommand : IStoreCardCommand<bool>
 {
+    /// <summary>
+    /// The action invoked if the user chooses to edit the item.
+    /// If this is null, there will be no option to edit the item.
+    /// </summary>
     private readonly Action? _editAction;
+
+    /// <summary>
+    /// The item we are trying to open.
+    /// </summary>
     private readonly SavedItem _item;
 
+    /// <summary>
+    /// Creates the command for a window with no option to edit the item.
+    /// </summary>
+    /// <param name="item">The item we are trying to open</param>
     public ShowMissingItemAlertCommand(SavedItem item) => _item = item;
 
+    /// <summary>
+    /// Creates the command for a window with an option to edit the item.
+    /// </summary>
+    /// <param name="item">The item we are trying to open</param>
+    /// <param name="editAction">The action invoked when the user chooses to edit the item</param>
     public ShowMissingItemAlertCommand(SavedItem item, Action editAction) : this(item) => _editAction = editAction;
 
+    /// <summary>
+    /// Opens the window. If the "Edit" button is available, the window is shown as a modal dialog,
+    /// and the command waits for the window to be closed. If there is no "Edit" button, the window is
+    /// opened normally without waiting for it to close.
+    /// </summary>
+    /// <returns>True</returns>
     public bool Execute()
     {
         if (_editAction != null)
