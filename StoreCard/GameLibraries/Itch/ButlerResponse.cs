65eea99ef11d6e68ace7b368ddf454e642d4db49
shanedonburke <shanedonburke@gmail.com>
2022-05-30 13:55:34 -0400

    Add docstrings

--- StoreCard/GameLibraries/Itch/ButlerResponse.cs
+++ StoreCard/GameLibraries/Itch/ButlerResponse.cs
@@ -6,13 +6,23 @@ using Newtonsoft.Json;
 
 namespace StoreCard.GameLibraries.Itch;
 
+/// <summary>
+/// Represents the JSON object returned as a response to Butler daemon requests.
+/// </summary>
+/// <typeparam name="TResult">Type of the <see cref="Result"/> property, which is request type-specific.</typeparam>
 public sealed class ButlerResponse<TResult>
 {
+    /// <summary>
+    /// The arbitrary ID we passed in with the request.
+    /// </summary>
     [JsonProperty("id")] public int Id;
 
-    [JsonProperty("jsonrpc")] public string JsonRpc;
-
+    /// <summary>
+    /// The request-specific result.
+    /// </summary>
     [JsonProperty("result")] public TResult Result;
+
+    [JsonProperty("jsonrpc")] public string JsonRpc;
 
     public ButlerResponse(string jsonRpc, int id, TResult result)
     {
