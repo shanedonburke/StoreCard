c59f169b6300cbd0ea6f72bc6478b4f30957b47b
shanedonburke <shanedonburke@gmail.com>
2022-05-30 15:59:58 -0400

    Add docstrings

--- StoreCard/Services/MessageBoxService.cs
+++ StoreCard/Services/MessageBoxService.cs
@@ -8,6 +8,9 @@ using StoreCard.Windows;
 
 namespace StoreCard.Services;
 
+/// <summary>
+/// A service to handle the display of message boxes.
+/// </summary>
 public sealed class MessageBoxService
 {
     public static readonly MessageBoxService Instance = new();
@@ -22,9 +25,19 @@ public sealed class MessageBoxService
     {
     }
 
+    /// <summary>
+    /// Show a message box.
+    /// </summary>
+    /// <param name="messageBoxText">The text to display</param>
+    /// <param name="caption">The window title/caption</param>
+    /// <param name="button">The button(s) to show</param>
+    /// <param name="icon">The icon to show in the window body</param>
     public void ShowMessageBox(string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon)
     {
+        // Get taskbar icon window if we don't have it
         _taskbarIconWindow ??= Application.Current.Windows.OfType<TaskbarIconWindow>().FirstOrDefault();
+        // If the window that shows the message box closes, so does the box.
+        // So, show it from a window that won't close.
         _taskbarIconWindow?.Dispatcher.BeginInvoke(() => MessageBox.Show(messageBoxText, caption, button, icon));
     }
 }
