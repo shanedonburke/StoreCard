859c2916083f3c0f1156bc8154d2a635109ce231
shanedonburke <shanedonburke@gmail.com>
2022-05-30 13:48:10 -0400

    Add docstrings

--- StoreCard/GameLibraries/Itch/ButlerClient.cs
+++ StoreCard/GameLibraries/Itch/ButlerClient.cs
@@ -16,16 +16,11 @@ namespace StoreCard.GameLibraries.Itch;
 /// <summary>
 /// An interface for interacting with the Butler daemon, which is used by the itch launcher
 /// for various purposes. We use it to get info about and launch installed games.
+/// We interact with the daemon through its JSON-RPC API, which we connect to with a TCP socket.
 /// See http://docs.itch.ovh/butlerd/master/#/ for documentation.
 /// </summary>
 public sealed class ButlerClient
 {
-    /// <summary>
-    /// The classes we've created for responses aren't complete, but that's okay
-    /// </summary>
-    private static readonly JsonSerializerSettings s_deserializeResponseSettings =
-        new() {MissingMemberHandling = MissingMemberHandling.Ignore};
-
     /// <summary>
     /// Path to the DB created by Butler.
     /// </summary>
@@ -49,7 +44,7 @@ public sealed class ButlerClient
     /// <summary>
     /// The Butler system process.
     /// </summary>
-    private Process _butlerProc;
+    private Process? _butlerProc;
 
     public void Start()
     {
@@ -59,6 +54,7 @@ public sealed class ButlerClient
             {
                 FileName = _execPath,
                 Arguments =
+                    // destiny-pid makes the daemon exit when StoreCard does
                     $"daemon --json --transport tcp --dbpath \"{_dbPath}\" --destiny-pid {Environment.ProcessId}",
                 UseShellExecute = false,
                 RedirectStandardOutput = true,
@@ -66,12 +62,15 @@ public sealed class ButlerClient
                 CreateNoWindow = true
             }
         };
+
         _butlerProc.Start();
 
+        // The notification printed by the daemon when it's ready
         ButlerListenNotification? listenNotif = null;
 
         while (listenNotif == null)
         {
+            // Each line of stdout is a JSON object
             string? jsonLine = _butlerProc.StandardOutput.ReadLine();
 
             if (jsonLine == null)
@@ -83,83 +82,120 @@ public sealed class ButlerClient
             {
                 listenNotif = JsonConvert.DeserializeObject<ButlerListenNotification>(
                     jsonLine,
+                    // If missing members are ignored, an object of a different type may "pass" as the
+                    // listen notifcation because it has some of the same members
                     new JsonSerializerSettings {MissingMemberHandling = MissingMemberHandling.Error});
             }
             catch (JsonSerializationException)
             {
+                // This line of stdout wasn't the notification, so try again
             }
         }
 
         _secret = listenNotif.Secret;
 
+        // Address in the form host:port
         string[] split = listenNotif.Tcp.Address.Split(":");
-        string server = split[0];
+        string host = split[0];
         int port = int.Parse(split[1]);
 
-        _socket.Connect(server, port);
+        _socket.Connect(host, port);
     }
 
+    /// <summary>
+    /// Send the authentication request. This must be called before any other request methods
+    /// (e.g., getting caves or launching games).
+    /// </summary>
+    /// <returns>True if the authentication succeeded, false if it did not</returns>
     public bool Authenticate()
     {
+        // We get the secret when we start the daemon
         if (_secret == null)
         {
             Start();
         }
 
         Dictionary<string, object> parameters = new() {{"secret", _secret!}};
-        return SendRequest<AuthenticateResult>(
-            Methods.MetaAuthenticate, parameters) != null;
+        return SendRequest<AuthenticateResult>(Methods.MetaAuthenticate, parameters) != null;
     }
 
+    /// <summary>
+    /// Gets the list of caves, i.e., info about installed games.
+    /// </summary>
+    /// <returns></returns>
     public List<ButlerCave>? FetchCaves() =>
         SendRequest<FetchCavesResult>(
             Methods.FetchCaves,
             new Dictionary<string, object>())?.Items;
 
+    /// <summary>
+    /// Launches a game.
+    /// </summary>
+    /// <param name="caveId">ID of the cave representing the game</param>
     public void Launch(string caveId)
     {
         Dictionary<string, object> parameters = new()
         {
             {"caveId", caveId}, {"prereqsDir", ButlerPaths.ButlerPrereqsFolder}
         };
+        // We don't care about the response
         SendRequest<object>(Methods.Launch, parameters);
     }
 
+    /// <summary>
+    /// Kills the Butler daemon process.
+    /// </summary>
     public void KillDaemon()
     {
-        _butlerProc.Kill();
+        _butlerProc?.Kill();
     }
 
+    /// <summary>
+    /// Sends a JSON-RPC request to the Butler daemon.
+    /// </summary>
+    /// <typeparam name="TResult">Type of the request-specific result</typeparam>
+    /// <param name="method">Name of the API method to invoke</param>
+    /// <param name="parameters">Parameters to be serialized into the request</param>
+    /// <returns></returns>
     private TResult? SendRequest<TResult>(string method, Dictionary<string, object> parameters)
     {
+        // Necessary parameters plus request-specific ones.
         Dictionary<string, object> req = new()
         {
+            // The ID doesn't matter, but it must be present
             {"jsonrpc", "2.0"}, {"method", method}, {"id", new Random().Next()}, {"params", parameters}
         };
 
-        string json = JsonConvert.SerializeObject(req);
-        byte[] utf8Bytes = Encoding.UTF8.GetBytes(json + "\n");
-        string ascii = Encoding.ASCII.GetString(utf8Bytes);
+        string json = JsonConvert.SerializeObject(req) + "\n";
 
-        _socket.Send(Encoding.ASCII.GetBytes(ascii));
+        // Strings are UTF-8 by default, but the daemon requires ASCII
+        _socket.Send(json.ToAsciiBytes());
 
         string response;
 
         do
         {
+            // Check for response every 100 ms.
             Thread.Sleep(100);
             byte[] buffer = new byte[65536];
             _socket.Receive(buffer);
             response = Encoding.ASCII.GetString(buffer);
         } while (response.Trim() == string.Empty);
 
-        // The buffer may contain multiple lines of JSON objects (e.g., logs)
+        // The buffer may contain multiple lines of JSON objects (e.g., logs + response).
+        // The first one will be the response.
         string resJson = response.Split('\n')[0];
 
         try
         {
+            // Return the result part of the response if we can deserialize it correctly.
+            // We are only sending one request at a time, so we don't need to worry about the wrong
+            // response being deserialized.
             return JsonConvert.DeserializeObject<ButlerResponse<TResult>>(
-                resJson, s_deserializeResponseSettings) is { } res
+                    resJson,
+                    // The response classes we've created may not be complete, but that's okay
+                    new JsonSerializerSettings {MissingMemberHandling = MissingMemberHandling.Ignore})
+                is { } res
                 ? res.Result
                 : default;
         }
@@ -170,6 +206,10 @@ public sealed class ButlerClient
         }
     }
 
+    /// <summary>
+    /// Names of the Butler daemon's API methods. The method name is specified in JSON-RPC requests
+    /// sent to the daemon.
+    /// </summary>
     private static class Methods
     {
         public const string MetaAuthenticate = "Meta.Authenticate";
@@ -179,6 +219,9 @@ public sealed class ButlerClient
         public const string Launch = "Launch";
     }
 
+    /// <summary>
+    /// Result of the authentication request.
+    /// </summary>
     private sealed class AuthenticateResult
     {
         [JsonProperty("ok")] public readonly bool Ok;
@@ -186,6 +229,9 @@ public sealed class ButlerClient
         public AuthenticateResult(bool ok) => Ok = ok;
     }
 
+    /// <summary>
+    /// Result of a request to fetch caves.
+    /// </summary>
     private sealed class FetchCavesResult
     {
         [JsonProperty("items")] public List<ButlerCave> Items;
--- StoreCard/Utils/Extensions.cs
+++ StoreCard/Utils/Extensions.cs
@@ -2,6 +2,7 @@
 
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Windows;
 using System.Windows.Media.Imaging;
 
@@ -21,6 +22,13 @@ public static class Extensions
     }
 
     public static string ToBase64(this BitmapSource bitmap) => ImageUtils.ImageToBase64(bitmap);
+
+    public static byte[] ToAsciiBytes(this string str)
+    {
+        byte[] utf8Bytes = Encoding.UTF8.GetBytes(str);
+        string asciiStr = Encoding.ASCII.GetString(utf8Bytes);
+        return Encoding.ASCII.GetBytes(asciiStr);
+    }
 
     public static void Deconstruct<T>(this IEnumerable<T> seq, out T? first, out IEnumerable<T> rest)
     {
