270169743094cd63d4545c4954bcff0cf8a88ece
shanedonburke <shanedonburke@gmail.com>
2022-05-30 12:42:28 -0400

    Add batch file comments

--- StoreCard/Effects/CompileShaders.bat
+++ StoreCard/Effects/CompileShaders.bat
@@ -1,6 +1,8 @@
+:: Compiles every shader file using the fxc program, which is expected to exist in the specified directory
 for %%f in (Effects\Shaders\*.fx) do (
   echo Compiling %%~nf.fx
   "%ProgramFiles(x86)%\Windows Kits\10\bin\10.0.22000.0\x86\fxc.exe" /O0 /Fc /Zi /T ps_3_0 /Fo Effects\bin\%%~nf.ps Effects\Shaders\%%~nf.fx
 )
+:: Delete intermediary files we don't need
 del Effects\Shaders\*.cod
 echo Finished compiling shaders.
