14e3703e9cc9cc5a0c1c843efa63512be87181ac
shanedonburke <shanedonburke@gmail.com>
2022-05-30 15:30:09 -0400

    Add docstrings

--- StoreCard/Models/Items/IListBoxItem.cs
+++ StoreCard/Models/Items/IListBoxItem.cs
@@ -7,13 +7,28 @@ using System.Windows.Media.Imaging;
 
 namespace StoreCard.Models.Items;
 
+/// <summary>
+/// Represents an item to be displayed in a <c>StoreCardListBox</c>.
+/// </summary>
 public interface IListBoxItem : IComparable<IListBoxItem>
 {
+    /// <summary>
+    /// Name/main item text.
+    /// </summary>
     public string Name { get; }
 
+    /// <summary>
+    /// Main icon.
+    /// </summary>
     public BitmapSource? BitmapIcon { get; }
 
+    /// <summary>
+    /// Optional secondary icon to show next to the main icon.
+    /// </summary>
     public BitmapSource? PrefixIcon { get; }
 
+    /// <summary>
+    /// secondary text to show for the item. Can be an empty string.
+    /// </summary>
     public string SecondaryText { get; }
 }
--- StoreCard/Models/UserConfig.cs
+++ StoreCard/Models/UserConfig.cs
@@ -8,19 +8,37 @@ using StoreCard.Utils;
 
 namespace StoreCard.Models;
 
+/// <summary>
+/// Represents the user's configuration of StoreCard.
+/// </summary>
 public class UserConfig
 {
+    /// <summary>
+    /// The modifier keys of the default hot key (Win+Shift+Z).
+    /// </summary>
     public static readonly uint DefaultHotKeyModifiers = HotKeyUtils.ModifiersToHotKeyByte(Key.LWin, Key.LeftShift);
 
+    /// <summary>
+    /// The action key of the default hot key (Win+Shift+Z).
+    /// </summary>
     public static readonly uint DefaultVirtualHotKey = HotKeyUtils.KeyToVirtualKey(Key.Z);
 
     private string _theme = "Lake (Dark)";
 
+    /// <summary>
+    /// Modifier keys for the current hot key.
+    /// </summary>
     public uint HotKeyModifiers;
 
-    public bool ShouldShowPrefixIcons = false;
-
+    /// <summary>
+    /// Virtual key for the current hot key (e.g., the virtual hot key for 'Z').
+    /// </summary>
     public uint VirtualHotKey;
+
+    /// <summary>
+    /// Whether prefix icons should be shown next to saved items.
+    /// </summary>
+    public bool ShouldShowPrefixIcons = false;
 
     public UserConfig()
     {
@@ -36,6 +54,9 @@ public class UserConfig
         ShouldShowTutorial = shouldShowTutorial;
     }
 
+    /// <summary>
+    /// Name of the theme. E.g. "Lake (Dark)" for <c>Lake (Dark).xaml</c>.
+    /// </summary>
     public string Theme
     {
         get => _theme;
@@ -46,13 +67,23 @@ public class UserConfig
         }
     }
 
+    /// <summary>
+    /// Whether the tutorial window should be shown on startup. Will be true
+    /// following installation until the user clicks "Don't show again".
+    /// </summary>
     public bool ShouldShowTutorial { get; private set; } = true;
 
+    /// <summary>
+    /// Reset the hot key to its default.
+    /// </summary>
     public void ResetHotKeyToDefault()
     {
         HotKeyModifiers = DefaultHotKeyModifiers;
         VirtualHotKey = DefaultVirtualHotKey;
     }
 
+    /// <summary>
+    /// Disable the tutorial window for future startups.
+    /// </summary>
     public void DisableTutorial() => ShouldShowTutorial = false;
 }
