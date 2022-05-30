fd48b393f3d31a64696cd20f54f4319626d13272
shanedonburke <shanedonburke@gmail.com>
2022-05-30 14:37:12 -0400

    Add docstring

--- StoreCard/GameLibraries/GameLibrary.cs
+++ StoreCard/GameLibraries/GameLibrary.cs
@@ -7,7 +7,14 @@ using StoreCard.Models.Items.Installed.Games;
 
 namespace StoreCard.GameLibraries;
 
+/// <summary>
+/// Represents the library of installed games for a game launcher.
+/// </summary>
 public abstract class GameLibrary
 {
+    /// <summary>
+    /// Get the games that are currently installed for this launcher.
+    /// </summary>
+    /// <returns>An enumerable of installed games</returns>
     public abstract IEnumerable<InstalledGame> GetInstalledGames();
 }
