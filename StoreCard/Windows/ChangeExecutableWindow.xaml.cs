cee9174e770ede6185e31a96a15f6a951210ea97
shanedonburke <shanedonburke@gmail.com>
2022-05-31 15:59:57 -0400

    Slight changes and comments

--- StoreCard/Windows/ChangeExecutableWindow.xaml.cs
+++ StoreCard/Windows/ChangeExecutableWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using StoreCard.Commands;
 using StoreCard.Models.Items.Saved.FileSystem;
@@ -18,7 +19,8 @@ using StoreCard.Utils;
 namespace StoreCard.Windows;
 
 /// <summary>
-/// Interaction logic for ChangeExecutableWindow.xaml
+/// A window that allows the user to select a new executable with which a
+/// file system item will be opened.
 /// </summary>
 public sealed partial class ChangeExecutableWindow : INotifyPropertyChanged
 {
@@ -30,6 +32,9 @@ public sealed partial class ChangeExecutableWindow : INotifyPropertyChanged
 
     private string _executableName = string.Empty;
 
+    /// <summary>
+    /// Whether <see cref="ShowDialog"/> was used to open the window.
+    /// </summary>
     private bool _isDialog;
 
     public ChangeExecutableWindow(SavedFileSystemItem item)
@@ -39,6 +44,9 @@ public sealed partial class ChangeExecutableWindow : INotifyPropertyChanged
         DataContext = this;
     }
 
+    /// <summary>
+    /// For executables, name of the selected executable.
+    /// </summary>
     public string ExecutableName
     {
         get => _executableName;
@@ -49,6 +57,9 @@ public sealed partial class ChangeExecutableWindow : INotifyPropertyChanged
         }
     }
 
+    /// <summary>
+    /// For executables, whether the entered path referes to a real file.
+    /// </summary>
     public bool DoesExecutableExist
     {
         get => _doesExecutableExist;
@@ -59,6 +70,9 @@ public sealed partial class ChangeExecutableWindow : INotifyPropertyChanged
         }
     }
 
+    /// <summary>
+    /// For executables, the icon derived from the file.
+    /// </summary>
     public ImageSource? ExecutableIcon
     {
         get => _executableIcon;
@@ -71,6 +85,10 @@ public sealed partial class ChangeExecutableWindow : INotifyPropertyChanged
 
     public event PropertyChangedEventHandler? PropertyChanged;
 
+    /// <summary>
+    /// Show the window as a dialog, remembering that it is a dialog.
+    /// </summary>
+    /// <returns></returns>
     public new bool? ShowDialog()
     {
         _isDialog = true;
@@ -87,6 +105,7 @@ public sealed partial class ChangeExecutableWindow : INotifyPropertyChanged
     {
         if (_isDialog)
         {
+            // DialogResult can only be set if this is a dialog
             DialogResult = false;
         }
 
@@ -97,21 +116,10 @@ public sealed partial class ChangeExecutableWindow : INotifyPropertyChanged
     private void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
-    private void SaveExecutableButton_Click(object sender, RoutedEventArgs e)
-    {
-        SetExecPathAndSave(ExecutablePathBox.Text);
-
-        Close();
-    }
-
-    private void BrowseButton_Click(object sender, RoutedEventArgs e)
-    {
-        if (new BrowseExecutableCommand().Execute() is { } filePath)
-        {
-            ExecutablePathBox.Text = filePath;
-        }
-    }
-
+    /// <summary>
+    /// Sets the executable path of the item to the selected app's executable path,
+    /// then saves it.
+    /// </summary>
     private void SaveSelectedAppAndClose()
     {
         if (AppSelector.SelectedApp == null)
@@ -121,6 +129,7 @@ public sealed partial class ChangeExecutableWindow : INotifyPropertyChanged
         }
         else
         {
+            // If we don't know the path to the selected app, use the default
             SetExecPathAndSave(AppSelector.SelectedApp.ExecutablePath
                                ?? SavedFileSystemItem.DefaultExecutable);
         }
@@ -128,6 +137,10 @@ public sealed partial class ChangeExecutableWindow : INotifyPropertyChanged
         Close();
     }
 
+    /// <summary>
+    /// Sets the executable path of the item and saves it.
+    /// </summary>
+    /// <param name="path">Path to the new executable</param>
     private void SetExecPathAndSave(string path)
     {
         SavedFileSystemItem? updatedItem = AppData.UpdateSavedItemById<SavedFileSystemItem>(
@@ -137,6 +150,19 @@ public sealed partial class ChangeExecutableWindow : INotifyPropertyChanged
         if (_isDialog)
         {
             DialogResult = updatedItem != null;
+        }
+    }
+
+    private void AppSelector_SaveButtonClick(object sender, RoutedEventArgs e) => SaveSelectedAppAndClose();
+
+    private void AppSelector_CancelButtonClick(object sender, RoutedEventArgs e) => Close();
+
+    private void BrowseButton_Click(object sender, RoutedEventArgs e)
+    {
+        // Set the path box text if a file was selected, triggering the TextChanged handler
+        if (new BrowseExecutableCommand().Execute() is { } filePath)
+        {
+            ExecutablePathBox.Text = filePath;
         }
     }
 
@@ -159,9 +185,21 @@ public sealed partial class ChangeExecutableWindow : INotifyPropertyChanged
         }
     }
 
-    private void AppSelector_SaveButtonClick(object sender, RoutedEventArgs e) => SaveSelectedAppAndClose();
+    private void SaveExecutableButton_Click(object sender, RoutedEventArgs e)
+    {
+        SetExecPathAndSave(ExecutablePathBox.Text);
 
-    private void AppSelector_CancelButtonClick(object sender, RoutedEventArgs e) => Close();
+        Close();
+    }
+
+    private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+    {
+        // Close the window if Escape is pressed
+        if (e.Key == Key.Escape)
+        {
+            Close();
+        }
+    }
 
     private void Window_Closed(object? sender, EventArgs e)
     {
